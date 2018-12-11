3b9e2f63b36c52c0cf0ef51612838c72a1329668
maximren <renoa325@gmail.com>
2018-12-11 21:42:21 +0200

    update config

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,2 +1,5 @@
 export const googleMapsApiKey = "AIzaSyCTbv6-mpOqcBaO0RUBZRmXElCDWK0pNOU";
+
 export const weatherApiKey = "c8e426cbb3864931b2b112415181112";
+
+export const weatherApi = "https://api.apixu.com/v1/forecast.json?key=";
