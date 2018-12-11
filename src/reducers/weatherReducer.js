964c45b000df04c22d8d791e1b582c3cbc0565a1
maximren <renoa325@gmail.com>
2018-12-11 21:45:21 +0200

    update wheather reducer

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,18 +1,17 @@
 const initialState = {
-	weatherItem: { current: { condition: {} } }
+  item: null
 }
 
-
 const weatherReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case 'GET_MY_CITY_WEATHER':
-			return {
-				...state,
-				weatherItem: action.payload
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'GET_MY_CITY_WEATHER':
+      return {
+        ...state,
+        item: action.payload
+      };
+    default:
+      return state;
+  }
 }
 
 export default weatherReducer;
