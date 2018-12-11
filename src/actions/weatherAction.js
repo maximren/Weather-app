86bcf9084da07927793506eaaeb1aa4e3c4022c9
maximren <renoa325@gmail.com>
2018-12-11 19:20:30 +0200

    add dynamic data download

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -1,9 +1,31 @@
+import getCityName from '../helpers/getCityName';
+import { googleMapsApiKey, weatherApiKey } from '../helpers/config';
+
+
+
 export const getWeather = () => dispatch => {
-	fetch("https://api.apixu.com/v1/forecast.json?key=c8e426cbb3864931b2b112415181112&q=Lviv")
+	navigator.geolocation.getCurrentPosition((data) => {
+		fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + data.coords.latitude + ',' + data.coords.longitude + '&sensor=true&key=' + googleMapsApiKey + '&language=en')
+			.then(result => result.json())
+			.then(data => {
+				fetch(`https://api.apixu.com/v1/forecast.json?key=` + weatherApiKey + `&q=${getCityName(data.results)}`)
+					.then(result => result.json())
+					.then(weather => dispatch({
+						type: 'GET_MY_CITY_WEATHER',
+						payload: weather
+					})
+					);
+			}).catch(err => console.log(err))
+	});
+}
+
+export const getOtherWeather = (city) => dispatch => {
+	fetch(`https://api.apixu.com/v1/forecast.json?key=` + weatherApiKey + `&q=${city}`)
 		.then(result => result.json())
 		.then(weather => dispatch({
-			type: 'GET_MY_CITY_WEATHER',
+			type: 'GET_OTHER_CITY_WEATHER',
 			payload: weather
-		})
-		);
+		}))
+		.catch(err => console.log(err));
 }
+
