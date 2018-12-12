70dc60e91b584b7925445766b9e39737d8093207
maximren <renoa325@gmail.com>
2018-12-12 14:16:19 +0200

    Updates

---
+++ precache-manifest.e93eb734458aaee3863a54437f7cdb0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ee90fef419b0337d269",
+    "url": "/Weather-app/static/js/runtime~main.6ee90fef.js"
+  },
+  {
+    "revision": "83d33f6ff86fb45f58ca",
+    "url": "/Weather-app/static/js/main.83d33f6f.chunk.js"
+  },
+  {
+    "revision": "3aa224587db2f00cd8c0",
+    "url": "/Weather-app/static/js/1.3aa22458.chunk.js"
+  },
+  {
+    "revision": "83d33f6ff86fb45f58ca",
+    "url": "/Weather-app/static/css/main.b62845e8.chunk.css"
+  },
+  {
+    "revision": "06356323841111aa2e46b3b3f4bb5edd",
+    "url": "/Weather-app/index.html"
+  }
+];
